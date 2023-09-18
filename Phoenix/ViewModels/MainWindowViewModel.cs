c2ca2a8a416ca668db7ac13059f093cdb8d18388
алексей загородников <ayayv.ayayayv@yandex.ru>
2023-09-18 16:20:18 +0300

    test

--- Phoenix/ViewModels/MainWindowViewModel.cs
+++ Phoenix/ViewModels/MainWindowViewModel.cs
@@ -11,28 +11,28 @@ namespace Phoenix.ViewModels
 {
     internal class MainWindowViewModel : ViewModelBase
     {
-        //#region Заголовок
-        //private string _title = "Главная страница";
-        //public string Title
-        //{
-        //    get => _title;
-        //    set => Set(ref _title, value);
-        //}
-        //#endregion
+        #region Заголовок
+        private string _title = "Главная страница";
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
 
-        //#region Текущая модель предстовления
+        #region Текущая модель предстовления
 
-        //private ViewModelBase _currentModel;
-        //public ViewModelBase CurrentModel
-        //{
-        //    get => _currentModel;
-        //    set => Set(ref _currentModel, value);
-        //}
-        //#endregion
+        private ViewModelBase _currentModel;
+        public ViewModelBase CurrentModel
+        {
+            get => _currentModel;
+            set => Set(ref _currentModel, value);
+        }
+        #endregion
 
-        //private readonly IRepository<Massage> _massages;
-        //private readonly IRepository<Client> _clients;
-        //private readonly IUserDialog<Client> _userDialog;
+        private readonly IRepository<Massage> _massages;
+        private readonly IRepository<Client> _clients;
+        private readonly IUserDialog<Client> _userDialog;
 
         public MainWindowViewModel(IRepository<Massage> massages, IRepository<Client> clients, IUserDialog<Client> _UserDialog)
         {
@@ -41,32 +41,32 @@ namespace Phoenix.ViewModels
             _userDialog = _UserDialog;
         }
 
-        //private ICommand _showMassageViewCommand;
+        private ICommand _showMassageViewCommand;
 
-        //public ICommand ShowMassageViewCommand => _showMassageViewCommand
-        //    ??= new CommandHelper(OnShowMassageViewCommandExecute, CanShowMassageViewCommandExecute);
+        public ICommand ShowMassageViewCommand => _showMassageViewCommand
+            ??= new CommandHelper(OnShowMassageViewCommandExecute, CanShowMassageViewCommandExecute);
 
-        //private bool CanShowMassageViewCommandExecute(object obj) => true;
+        private bool CanShowMassageViewCommandExecute(object obj) => true;
 
-        //private void OnShowMassageViewCommandExecute(object obj)
-        //{
-        //    CurrentModel = new MassageViewModel(_massages);
-        //}
+        private void OnShowMassageViewCommandExecute(object obj)
+        {
+            CurrentModel = new MassageViewModel(_massages);
+        }
 
-        //#region Команда покказа окна клиентов
+        #region Команда покказа окна клиентов
 
-        //private ICommand _showClientsViewCommand;
+        private ICommand _showClientsViewCommand;
 
-        //public ICommand ShowClientsViewCommand => _showClientsViewCommand
-        //    ??= new CommandHelper(OnShowClientsViewCommandExecuted, CanShowClientsViewCommandExecute);
+        public ICommand ShowClientsViewCommand => _showClientsViewCommand
+            ??= new CommandHelper(OnShowClientsViewCommandExecuted, CanShowClientsViewCommandExecute);
 
-        //private bool CanShowClientsViewCommandExecute(object obj) => true;
+        private bool CanShowClientsViewCommandExecute(object obj) => true;
 
-        //private void OnShowClientsViewCommandExecuted(object obj)
-        //{
-        //    CurrentModel = new ClientsViewModel(_clients, _userDialog);
-        //}
+        private void OnShowClientsViewCommandExecuted(object obj)
+        {
+            CurrentModel = new ClientsViewModel(_clients, _userDialog);
+        }
 
-        //#endregion
+        #endregion
     }
 }
